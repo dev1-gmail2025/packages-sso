21e9638fd152a8c30abc134db642c60d1dbf4aca
dev1-gmail2025 <sevago.dev1@gmail.com>
2026-03-24 13:35:52 +0700

    feat: export table

---
+++ src/components/elements/table/index.ts
@@ -0,0 +1,7 @@
+export * from './table-collapse.component';
+export * from './table.argtypes';
+export * from './table.component';
+export * from './table.constant';
+export * from './table.enum';
+export * from './table.interface';
+
