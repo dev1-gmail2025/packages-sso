4317435f8b9e90026c98a4f1317dc4e1eee7a7e4
dev1-gmail2025 <sevago.dev1@gmail.com>
2026-04-24 13:42:08 +0700

    feat: build date 2026-04-24

---
+++ docs/assets/button-icon.element.stories-BK7orl06.js
@@ -0,0 +1,40 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{f as T,w as k,u as B,e as S}from"./index-DtL3pAzF.js";import{B as o}from"./button-icon.element-BNHwqEfC.js";import"./time-ago-content.component-CND7BupQ.js";import{a as n}from"./image.element-BzCya6VX.js";import{b as j}from"./ckeditor-CTLCbsNz.js";import"./time.util-BYguZ9lS.js";import"./typography.style-CkWB8eZ1.js";import"./stack.style-yVtBqaKj.js";import"./avatar-group.element-DqOG9HI5.js";import"./avatar-online-status.element-W8gsYIgI.js";import"./avatar-user-info-detail.element-fcGRxM02.js";import"./avatar-user-info-update.element-rsH5SLEz.js";import"./avatar-user-info.element-D4CSZD_E.js";import"./avatar.element-DnKBsB0B.js";import"./time-ago.component-CxAZJ-Qa.js";import{S as w}from"./Stack-PxG0HRcj.js";import"./loading.component-EnOu9Ri6.js";import"./CircularProgress-SwCU_jrE.js";import"./index-j62jqFia.js";import"./DefaultPropsProvider-BwONcEM9.js";import"./defaultTheme-CAzuqlo8.js";import"./memoTheme-JcuT4PI6.js";import"./createSimplePaletteValueFilter-bm0fmN_7.js";import"./extendSxProp-BhkXlpcg.js";import"./icon-button.element-CeM9eyxq.js";import"./icon.element-D4jnsA1U.js";import"./useTheme--firQ3HK.js";import"./useId-CFSqmtgZ.js";import"./ButtonBase-DMmz3Ngg.js";import"./useTimeout-BnjI5Ij7.js";import"./useEnhancedEffect-Dmr-a5D-.js";import"./TransitionGroupContext-DM7DmuUd.js";import"./useForkRef-BVpE-hzr.js";import"./isFocusVisible-B8k4qzLc.js";import"./icon-content-badge-subs.element-cCXQ9PVw.js";import"./link.element-BIiOul_5.js";import"./Typography-Bf8WTvH-.js";import"./index-F4SWrtxx.js";import"./tooltip-on-click.element-DkCofoav.js";import"./getReactElementRef-BVRziQZO.js";import"./index-CWS0_3Nh.js";import"./index-Ctr1kwqx.js";import"./Grow-DOIJXnb6.js";import"./useSlot-BHAemyrm.js";import"./Tooltip-C-jnBW1Q.js";import"./index-DP7gAqwz.js";import"./useControlled-CdETZoY_.js";import"./Box-CJND6-sR.js";import"./icon-content-badge.element-CGf3sbLu.js";import"./icon-content.element-DlSOyn8V.js";import"./other.util-D3ae_aVB.js";import"./Badge-4F4pJWAG.js";import"./ownerWindow-SaFBzm43.js";import"./createSvgIcon-CgF2gLAa.js";import"./proxy-DKXG_8Nh.js";import"./Stack-BWemv0CO.js";import"./createStack-_FjnXW6X.js";import"./Button-oCoBaHSt.js";import"./typography-content-caption.component-BwLO197l.js";import"./typography-grid-info-row.component-CZtKs3pG.js";import"./typography-info-row.component-BX5ZGiZP.js";import"./typography-info-user.component-KVcKJon1.js";import"./text-field-date.element-h1fW62DV.js";import"./picker.style-CmQB8-cu.js";import"./text-field-label.element-DV8iDHXQ.js";import"./DatePicker-s9R1gce8.js";import"./useMobilePicker-DJGbGygp.js";import"./Fade-NGFjKnxD.js";import"./Popover-Dcprfih5.js";import"./mergeSlotProps-C1T8RA4K.js";import"./createChainedFunction-BO_9K8Jh.js";import"./InputLabel-Xf4L_974.js";import"./useFormControl-CmOg5wnR.js";import"./TextField-BEYRgNgZ.js";import"./FormControl-CkqPUPi_.js";import"./isMuiElement-BY4WSVWD.js";import"./List-sl9rBPBu.js";import"./ListItem-75WI95rP.js";import"./Chip-XHsgVC5o.js";import"./text-field-date-range.element-DoxtZfBO.js";import"./text-field-date-time.element-q8G99WTQ.js";import"./MenuItem-BrS6X00U.js";import"./text-field-email-autocomplete.element-C3dkiiPV.js";import"./Autocomplete-U4_bzeDZ.js";import"./text-field-number.element-CwFjnuGY.js";import"./text-field-select.element-DP8St7IO.js";import"./empty.component-DQWAoh-q.js";import"./text-field-select-search-object.element-Y-9BLkT8.js";import"./text-highlight.element-MWkeHQEn.js";import"./string.util-DkuOPhSi.js";import"./text-field.element-CftLMsJY.js";import"./typography-limit-one-line.component-7Je5dHj4.js";const z={icon:{control:"text"},loading:{control:"boolean"},disabled:{control:"boolean"},variant:{control:"select",options:["text","outlined","contained"]},color:{control:"select",options:["inherit","primary","secondary","success","error","info","warning"]},size:{control:"select",options:["small","medium","large"]},sizeType:{control:"select",options:Object.values(n)},onClick:{action:"clicked"}},gr={title:"Elements/Button/ButtonIconElement",component:o,tags:["autodocs"],args:{icon:"edit",onClick:T(),variant:"outlined",color:"primary"},parameters:{layout:"centered"},argTypes:z},i={},e={render:r=>t.jsxs(w,{gap:j,children:[t.jsxs("div",{style:{display:"flex",gap:16,flexWrap:"wrap"},children:[t.jsx(o,{...r,variant:"contained"}),t.jsx(o,{...r,variant:"outlined"}),t.jsx(o,{...r,variant:"text"})]}),t.jsxs("div",{style:{display:"flex",gap:16,flexWrap:"wrap"},children:[t.jsx(o,{...r,variant:"contained",sizeType:n.CIRCLE}),t.jsx(o,{...r,variant:"outlined",sizeType:n.CIRCLE}),t.jsx(o,{...r,variant:"text",sizeType:n.CIRCLE})]})]})},a={args:{loading:!0},parameters:{chromatic:{delay:300}}},p={play:async({canvasElement:r,args:I})=>{const f=k(r);await B.click(f.getByRole("button")),await S(I.onClick).toHaveBeenCalledTimes(1)}};var m,s,c;i.parameters={...i.parameters,docs:{...(m=i.parameters)==null?void 0:m.docs,source:{originalSource:"{}",...(c=(s=i.parameters)==null?void 0:s.docs)==null?void 0:c.source}}};var l,d,u;e.parameters={...e.parameters,docs:{...(l=e.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: args => <Stack gap={STYLE.PADDING_GAP_LAYOUT}>\r
+      <div style={{
+      display: 'flex',
+      gap: 16,
+      flexWrap: 'wrap'
+    }}>\r
+        <ButtonIconElement {...args} variant="contained" />\r
+        <ButtonIconElement {...args} variant="outlined" />\r
+        <ButtonIconElement {...args} variant="text" />\r
+      </div>\r
+      <div style={{
+      display: 'flex',
+      gap: 16,
+      flexWrap: 'wrap'
+    }}>\r
+        <ButtonIconElement {...args} variant="contained" sizeType={ImageSizeType.CIRCLE} />\r
+        <ButtonIconElement {...args} variant="outlined" sizeType={ImageSizeType.CIRCLE} />\r
+        <ButtonIconElement {...args} variant="text" sizeType={ImageSizeType.CIRCLE} />\r
+      </div>\r
+    </Stack>
+}`,...(u=(d=e.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};var g,y,v;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    loading: true
+  },
+  parameters: {
+    chromatic: {
+      delay: 300
+    }
+  }
+}`,...(v=(y=a.parameters)==null?void 0:y.docs)==null?void 0:v.source}}};var x,C,E;p.parameters={...p.parameters,docs:{...(x=p.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  play: async ({
+    canvasElement,
+    args
+  }) => {
+    const canvas = within(canvasElement);
+    await userEvent.click(canvas.getByRole('button'));
+    await expect(args.onClick).toHaveBeenCalledTimes(1);
+  }
+}`,...(E=(C=p.parameters)==null?void 0:C.docs)==null?void 0:E.source}}};const yr=["Default","Variants","Loading","ClickCallsOnClick"];export{p as ClickCallsOnClick,i as Default,a as Loading,e as Variants,yr as __namedExportsOrder,gr as default};
