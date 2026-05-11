4e8371d6c9161f11fa32898632888e7e0feab7e6
dev1-gmail2025 <sevago.dev1@gmail.com>
2026-05-11 09:51:16 +0700

    feat: build date 2026-05-11

--- src/components/elements/typography/typography-info-user/typography-info-user.component.tsx
+++ src/components/elements/typography/typography-info-user/typography-info-user.component.tsx
@@ -1,26 +1,29 @@
 import { SxProps, Theme, Typography } from '@mui/material';
+import { Stack } from '@mui/system';
 import React from 'react';
-import { COLOR, STYLE } from '../../../../common';
+import { STYLE, COLOR } from '../../../../common';
 import { StackRow } from '../../../styles';
 import { highlightYellow } from '../../text-field';
-import { Stack } from '@mui/system';
+import { TagElement } from '../../tag';
 
 export interface InfoItem {
   label: string;
   value: string | React.ReactNode;
   sxValue?: SxProps<Theme>;
+  statusTag?: boolean;
 }
 
 export interface TypographyInfoGridProps {
   items: InfoItem[];
   columns?: number;
   sx?: SxProps<Theme>;
-  columnGap?: number;
+  columnGap?: string | number;
   showEmptyAsDots?: boolean;
   gridTemplateColumns?: string;
-  gap?: number;
+  gap?: number | string;
   highlightQuery?: string;
   direction?: 'row' | 'column';
+  color?: string;
 }
 
 export const TypographyInfoUser: React.FC<TypographyInfoGridProps> = ({
@@ -33,6 +36,7 @@ export const TypographyInfoUser: React.FC<TypographyInfoGridProps> = ({
   gridTemplateColumns = '1.75fr 2fr',
   highlightQuery,
   direction = 'row',
+  color,
 }) => {
   return (
     <StackRow
@@ -48,34 +52,29 @@ export const TypographyInfoUser: React.FC<TypographyInfoGridProps> = ({
         <Stack
           key={index}
           sx={{
-            display: direction === 'row' ? 'grid' : 'flex',
             gridTemplateColumns: direction === 'row' ? (columns === 1 ? '1fr 3fr' : gridTemplateColumns) : 'unset',
             height: 'fit-content',
             flexDirection: direction === 'column' ? 'column' : 'row',
+            gap: 0,
           }}
         >
-          <Typography
-            sx={{
-              ...STYLE.TYPOGRAPHY.textSm.regular,
-              color: COLOR.COMMON.gray6,
-              flex: 1,
-            }}
-          >
+          <Typography sx={{ ...STYLE.TYPOGRAPHY.textSm.regular, color: COLOR.COMMON.gray6, flex: 1 }}>
             {highlightYellow(item.label, highlightQuery ?? '', showEmptyAsDots)}:
           </Typography>
 
-          <Typography
-            sx={{
-              ...STYLE.TYPOGRAPHY.textSm.regular,
-              color: COLOR.COMMON.black,
-              flex: 1,
-              borderBottom: `1px solid ${COLOR.COMMON.gray8}`,
-              textAlign: 'right',
-              ...item.sxValue,
-            }}
-          >
-            {highlightYellow(item.value, highlightQuery ?? '', showEmptyAsDots)}
-          </Typography>
+          {item.statusTag ? (
+            <TagElement
+              sx={{ textTransform: 'none', width: '100%', ...sx }}
+              content={item.value as string}
+              type={color || 'inherit'}
+            />
+          ) : (
+            <Typography
+              sx={{ ...STYLE.TYPOGRAPHY.textSm.regular, color: COLOR.COMMON.black, flex: 1, ...item.sxValue }}
+            >
+              {highlightYellow(item.value, highlightQuery ?? '', showEmptyAsDots)}
+            </Typography>
+          )}
         </Stack>
       ))}
     </StackRow>
